2b647024040117626a625b5c47768f042f686143
RobinAldrin <106313795+RobinAldrin@users.noreply.github.com>
2022-05-27 13:58:07 +0530

    Update Testing_app.py

--- Testing_app.py
+++ Testing_app.py
@@ -1,2 +1,14 @@
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+import pandas as pd
+
+student_dict = {'Name': ['Joe', 'Nat', 'Harry'], 'Age': [20, 21, 19], 'Marks': [85.10, 77.80, 91.54]}
+
+# create DataFrame from dict
+student_df = pd.DataFrame(student_dict)
+print(student_df)
+
+# set index using column
+student_df = student_df.set_index('Name')
+print(student_df)
