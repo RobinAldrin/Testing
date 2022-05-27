2da3b466ab52eac88171138fdd260dac4959dab3
RobinAldrin <106313795+RobinAldrin@users.noreply.github.com>
2022-05-27 14:17:42 +0530

    Update Testing_app.py

--- Testing_app.py
+++ Testing_app.py
@@ -3,6 +3,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 #my_fruit_list.setindex('Fruit' , inplace = True)
 
 # Put a Pick-up list
-streamlit.multiselect("Pick some fruits: " , list (my_fruit_list.index))
+#streamlit.multiselect("Pick some fruits: " , list (my_fruit_list.index))
 # Table Display
 streamlit.dataframe(my_fruit_list)
